533576aed2a6b7884c40de67c357dc2c943692de
JuanCruz2 <49735921+JuanCruz2@users.noreply.github.com>
2021-06-08 16:33:04 -0300

    feat: grid layout for heroes card display

---
+++ src/pages/home/components/header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyledHeader } from './styled'
+import MarvelLogo from '../../../../assets/img/marvel-logo.png'
+import { StyledInput } from '../../../../styles/globalStyles'
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <img src={MarvelLogo} alt="logo" />
+            <StyledInput placeholder="Search..." />
+        </StyledHeader>
+    )
+}
---
+++ src/pages/home/components/heroe-card/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {StyledCard} from "./styled"
+
+export const HeroeCard = () => {
+    return (
+        <StyledCard>
+            card
+        </StyledCard>
+    )
+}
